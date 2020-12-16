57cb58b883fcde79724f3fa505548fffe2e0e7a5
lhy0128 <1005447927@qq.com>
2020-12-16 19:43:51 +0800

    master

--- src/view/topPart/Login.vue
+++ src/view/topPart/Login.vue
@@ -1,25 +1,37 @@
 <template>
-  <div>
-   <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
-  <el-form-item label="密码" prop="pass">
-    <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
-  </el-form-item>
-  <el-form-item label="确认密码" prop="checkPass">
-    <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
-  </el-form-item>
-  <el-form-item label="年龄" prop="age">
-    <el-input v-model.number="ruleForm.age"></el-input>
-  </el-form-item>
-  <el-form-item>
-    <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
-    <el-button @click="resetForm('ruleForm')">重置</el-button>
-  </el-form-item>
-  </el-form>
+<div class="all">
+  <div class="left">
+    <img src="@/assets/registerl.png" alt="">
   </div>
+  <div class="right">
+    <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+      <el-form-item label="名称" prop="name">
+        <el-input v-model="ruleForm.name"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="pass">
+        <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="确认密码" prop="checkPass">
+       <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="年龄" prop="age">
+        <el-input v-model.number="ruleForm.age"></el-input>
+      </el-form-item>
+      <el-form-item prop="email" label="邮箱">
+        <el-input v-model="ruleForm.email"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+        <el-button @click="resetForm('ruleForm')">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</div>
 </template>
 
 <script>
 export default {
+  name: 'register',
   data () {
     var checkAge = (rule, value, callback) => {
       if (!value) {
@@ -47,18 +59,40 @@ export default {
         callback()
       }
     }
+    var validatePass2 = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请再次输入密码'))
+      } else if (value !== this.ruleForm.pass) {
+        callback(new Error('两次输入密码不一致!'))
+      } else {
+        callback()
+      }
+    }
     return {
       ruleForm: {
+        name: '',
         pass: '',
         checkPass: '',
-        age: ''
+        age: '',
+        email: ''
       },
       rules: {
+        name: [
+          { required: true, message: '请输入活动名称', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ],
         pass: [
-          { validator: validatePass, trigger: 'blur' }
+          { required: true, validator: validatePass, trigger: 'blur' }
+        ],
+        checkPass: [
+          { required: true, validator: validatePass2, trigger: 'blur' }
         ],
         age: [
           { validator: checkAge, trigger: 'blur' }
+        ],
+        email: [
+          { message: '请输入邮箱地址', trigger: 'blur' },
+          { type: 'email', message: '请输入正确的邮箱地址', trigger: ['blur', 'change'] }
         ]
       }
     }
@@ -80,13 +114,19 @@ export default {
   }
 }
 </script>
-<style  scoped>
-.form {
-  width: 400px;
-  margin: 100px auto;
-  border: 2px solid lavender;
-  padding: 100px 50px;
-  border-radius: 5px;
-  background-color: peachpuff
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.all{
+  margin-top: 10%;
+  width: 100%;
+}
+.left{
+  float: left;
+  width: 50%;
+}
+.right{
+  float: left;
+  width: 30%;
 }
 </style>
